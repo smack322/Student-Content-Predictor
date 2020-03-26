0afb74525e52726118da9ba5a465640c582df826
Scott <smcanally11@yahoo.com>
2020-03-25 20:09:01 -0400

    add delete and edit buttons for each student div

--- public/js/student.js
+++ public/js/student.js
@@ -25,23 +25,54 @@ $('#student-submit').on('click', function(event) {
 
 var updateBtn = $("<button type=submit> Update</button>");
 var deleteBtn = $("<button type=submit> X </button>");
+var studentContainer = $("#student-data");
 
 $.get('/api/all/', function(data) {
+    posts = data;
     console.log('data' + data);
-    if (data.length !== 0) {
-        for (var i = 0; i < data.length; i++) {
-            var row = $("<div>");
-            var button = $("<div>")
-
-            row.append(updateBtn);
-            row.append(deleteBtn);
-            row.append("<p>" + data[i].studentName);
-            row.append("<p>" + data[i].classCode);
-            row.append("<p>" + data[i].gradDate);
-            row.append("<p>" + data[i].classType);
-
-            $('#student-data').prepend(row);
-        }
+    if(!posts || !posts.length) {
+        noStudents();
+    }
+    else {
+        startRow();
     }
 });
+
+function startRow() {
+    var postsToCreate = [];
+
+    for (var i = 0; i < posts.length; i++) {
+        postsToCreate.push(createStudentRow(posts[i]));
+    }
+    studentContainer.append(postsToCreate);
+
+}
+
+function createStudentRow(data) {
+    var row = $("<div>");
+    var deleteBtn = $("<button>");
+    deleteBtn.text("X");
+    var editBtn = $("<button>");
+    editBtn.text("EDIT");
+    //append children to div
+    row.append(deleteBtn);
+    row.append(editBtn);
+    
+    var paragraph = $(`<p>${data}`);
+    row.append(`<p> ${data.studentName}`);
+    row.append(`<p> ${data.classCode}`);
+    row.append(`<p> ${data.gradDate}`);
+    row.append(`<p> ${data.classType}`);
+    
+
+    return studentContainer.prepend(row);
+    
+}
+
+function noStudents() {
+    studentContainer.empty();
+    var title = $("<h2>")
+    title.text("To add a new student use the form above.");
+    studentContainer.append(title);
+}
 
--- schema.sql
+++ schema.sql
@@ -1,2 +1,5 @@
 DROP DATABASE IF EXISTS `students`;
 CREATE DATABASE `students`;
+
+INSERT INTO students (id, studentName, classCode, classType, createdAt, updatedAt)
+VALUES (1, "Scott", "9a9waea9das", "Part-time", 3/16/20, 3/16/20);
